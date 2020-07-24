71c85636448b22da761e40d0155d7e37786c4258
CryllicBuster273 <cryllicbuster273@pixelexperience.org>
2020-07-25 02:13:04 +0530

    phoenix: PixelExperience Bringup
    Signed-off-by: CryllicBuster273 <cryllicbuster273@pixelexperience.org>

--- aosp_phoenix.mk
+++ aosp_phoenix.mk
@@ -6,12 +6,18 @@
 
 $(call inherit-product, device/xiaomi/phoenix/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common PixelExperience stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_phoenix
+PRODUCT_NAME := aosp_phoenix
 PRODUCT_DEVICE := phoenix
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi K30
 PRODUCT_MANUFACTURER := Xiaomi
+
+# PixelExperience Properties
+TARGET_GAPPS_ARCH := arm64
+TARGET_BOOT_ANIMATION_RES := 1080
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
