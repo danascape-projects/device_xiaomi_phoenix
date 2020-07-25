677a5d60dabc4f8b503e763716ae903b9cdec6c6
CryllicBuster273 <cryllicbuster273@pixelexperience.org>
2020-07-25 15:26:57 +0530

    phoenix: Use Coral Build FP
    Signed-off-by: CryllicBuster273 <cryllicbuster273@pixelexperience.org>

--- init/init_phoenix.cpp
+++ init/init_phoenix.cpp
@@ -67,13 +67,6 @@ constexpr const char *BUILD_DESCRIPTION[] = {
     "phoenixin-user 10 QKQ1.190825.002 V11.0.11.0.QGHINXM release-keys",
 };
 
-constexpr const char *BUILD_FINGERPRINT[] = {
-    "Redmi/phoenix/phoenix:10/QKQ1.190825.002/V12.0.3.0.QGHCNXM:user/"
-    "release-keys",
-    "POCO/phoenixin/phoenixin:10/QKQ1.190825.002/V11.0.11.0.QGHINXM:user/"
-    "release-keys",
-};
-
 constexpr const char *CLIENT_ID[] = {
     "android-xiaomi",
     "android-xiaomi-rev1",
@@ -111,11 +104,9 @@ void load_props(const char *model, bool is_in = false) {
     if (!is_in) {
       ro_prop_override(source, "brand", BRANDS[0], true);
       ro_prop_override(source, "name", PRODUCTS[0], true);
-      ro_prop_override(source, "fingerprint", BUILD_FINGERPRINT[0], false);
     } else {
       ro_prop_override(source, "brand", BRANDS[1], true);
       ro_prop_override(source, "name", PRODUCTS[1], true);
-      ro_prop_override(source, "fingerprint", BUILD_FINGERPRINT[1], false);
     }
   }
 
