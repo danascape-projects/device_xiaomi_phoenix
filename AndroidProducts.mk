2d6bf205fbfb94d59fcdf7f13caf95c4ac8be7b0
Palakurthi Sreekanth <palakurthisreekanth995@gmail.com>
2020-11-18 09:52:46 +0100

    phoenix: arrow-ify
    Signed-off-by: Palakurthi Sreekanth <palakurthisreekanth995@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/derp_phoenix.mk
-
-COMMON_LUNCH_CHOICES := \
-    derp_phoenix-user \
-    derp_phoenix-userdebug \
-    derp_phoenix-eng
+    $(LOCAL_DIR)/arrow_phoenix.mk
--- arrow_phoenix.mk
+++ arrow_phoenix.mk
@@ -6,20 +6,20 @@
 
 $(call inherit-product, device/xiaomi/phoenix/device.mk)
 
-# Inherit some common PixysOS stuff.
-$(call inherit-product, vendor/derp/config/common_full_phone.mk)
+# Inherit some common ArrowOS stuff.
+$(call inherit-product, vendor/arrow/config/common.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := derp_phoenix
+PRODUCT_NAME := arrow_phoenix
 PRODUCT_DEVICE := phoenix
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi K30
 PRODUCT_MANUFACTURER := Xiaomi
 
-# PixysOS Properties
+# ArrowOS Properties
 TARGET_GAPPS_ARCH := arm64
 TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_SUPPORTS_GOOGLE_RECORDER := true
+DEVICE_MAINTAINER := Sreekanth
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DISC="coral-user 11 RP1A.201105.002 6869500 release-keys"
